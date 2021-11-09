f8bc8e2f59dacc064d7b1cb3215f28ed6a77a7b8
Ádám Sándor <10675791+adam-sandor@users.noreply.github.com>
2021-11-09 13:12:20 +0100

    add account controller

---
+++ banking-api/src/main/java/superbank/backend/AuthHeader.java
@@ -0,0 +1,12 @@
+package superbank.backend;
+
+public class AuthHeader {
+
+    public static String getBearerToken(String authHeader) {
+        String[] authSplit = authHeader.split(" ");
+        if (!"Bearer".equals(authSplit[0]) || authSplit.length != 2) {
+            throw new IllegalStateException("Unknown Authorization header format: " + authHeader);
+        }
+        return authSplit[1];
+    }
+}
---
+++ banking-api/src/main/java/superbank/backend/EntitlementsController.java
@@ -0,0 +1,38 @@
+package superbank.backend;
+
+import com.bisnode.opa.client.OpaClient;
+import com.bisnode.opa.client.query.QueryForDocumentRequest;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class EntitlementsController {
+
+	private final OpaClient opaClient;
+
+	private static final Logger log = LoggerFactory.getLogger(EntitlementsController.class);
+
+	public EntitlementsController(@Autowired OpaClient opaClient) {
+		this.opaClient = opaClient;
+	}
+
+	@GetMapping("/entitlements")
+	JsonNode entitlements(@RequestHeader(name = "Authorization") String authHeader) {
+		String token = AuthHeader.getBearerToken(authHeader);
+
+		ObjectNode input = new ObjectMapper().createObjectNode();
+		input.put("jwt", token);
+
+		ObjectNode results = opaClient.queryForDocument(new QueryForDocumentRequest(input, "entitlements"), ObjectNode.class);
+		return results.get("entitlements");
+	}
+
+
+}

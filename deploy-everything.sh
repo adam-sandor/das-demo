23004169c930bff2d7e0348d4f0a4cda8aac28aa
Ádám Sándor <10675791+adam-sandor@users.noreply.github.com>
2021-11-12 18:01:35 +0100

    build images

--- deploy-everything.sh
+++ deploy-everything.sh
@@ -1,7 +1,5 @@
 NS=microservice-demo
 
-kubectl create configmap opa-config --from-file=opa-conf.yaml=config/opa-conf-entitlements.yaml -n $NS
-
 kubectl apply -f banking-api/deployment.yaml -n $NS
 kubectl apply -f frontend-portal/deployment.yaml -n $NS
 kubectl apply -f status-service/deployment.yaml -n $NS

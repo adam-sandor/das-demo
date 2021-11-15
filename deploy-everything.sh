42b3c7407a5aee0bd3d33b41e488df33a3a00834
Ádám Sándor <10675791+adam-sandor@users.noreply.github.com>
2021-11-15 12:49:58 +0100

    add deleting all pods to deployment script

--- deploy-everything.sh
+++ deploy-everything.sh
@@ -5,3 +5,6 @@ kubectl apply -f frontend-portal/deployment.yaml -n $NS
 kubectl apply -f status-service/deployment.yaml -n $NS
 
 kubectl apply -f k8s/ingress.yaml -n $NS
+
+#we rely on ImagePullPolicy: Always for redeployment of images
+kubectl delete pod -l app.kubernetes.io/name=das-microservice-demo

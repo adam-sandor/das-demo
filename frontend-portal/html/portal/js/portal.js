bbf30f8d5c7442b0150e09bf00e65f43b354b1b5
Ádám Sándor <10675791+adam-sandor@users.noreply.github.com>
2021-12-07 15:02:50 +0100

    display error when loading of entitlements fails

--- frontend-portal/html/portal/js/portal.js
+++ frontend-portal/html/portal/js/portal.js
@@ -1,24 +1,27 @@
 function readyFn() {
     $.ajax({
         type: "GET",
-        beforeSend: function (request) {
-            request.setRequestHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjEzMDA4MTkzODAsImlzcyI6Imp3dC5pbyIsInJvbGUiOiJjdXN0b21lcl9zdXBwb3J0Iiwic3ViIjoiSmFuZSBEb2UiLCJ1c2VySWQiOiIxMjM0Iiwicm9sZV9sZXZlbCI6IjIifQ.TLb16PIk9TwUySf8ieGd5OGlehBkvUVX_XDCOOpd_zY");
-        },
         url: "/entitlements",
-        success: entitlementsReady
+        success: entitlementsReady,
+        error: entitlementsCallError
     })
 }
 
 function entitlementsReady(data) {
     if (!data.includes('account/status')) {
-        $("#account-status").remove()
+        $("#account-status").show()
     }
     if (!data.includes('account/transactions')) {
-        $("#account-transactions").remove()
+        $("#account-transactions").show()
     }
     if (!data.includes('account/modify')) {
-        $("#account-modify").remove()
+        $("#account-modify").show()
     }
+}
+
+function entitlementsCallError(data) {
+   $("#error").show();
+   $("#error .error-text").text("Failed to load entitlements (" + data.statusText + " [" + data.status + "])");
 }
 
 $(readyFn)

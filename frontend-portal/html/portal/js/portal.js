9f4e38501d30d73bcd5ea806856e4f778cac2b0c
Ádám Sándor <10675791+adam-sandor@users.noreply.github.com>
2021-12-13 15:40:31 +0100

    ui can load transactions and account details

--- frontend-portal/html/portal/js/portal.js
+++ frontend-portal/html/portal/js/portal.js
@@ -14,7 +14,7 @@ function entitlementsReady(data) {
     if (data.entitlements.includes('account/transactions')) {
         $("#account-transactions").show()
     }
-    if (data.entitlements.includes('account/modify')) {
+    if (data.entitlements.includes('account/block')) {
         $("#account-modify").show()
     }
     $('#user-full-name').text(data.subject.fullname);
@@ -27,6 +27,88 @@ function entitlementsCallError(data) {
    $("#error").show();
    $("#error .error-text").text("Failed to load entitlements (" + data.statusText + " [" + data.status + "])");
 }
+
+$('#account-details').click(function () {
+    const ready = function (data) {
+        $("#error").hide();
+        const htmlTable = `<table class="table table-bordered">
+            <tbody>
+            <tr>
+                <td>IBAN:</td>
+                <td>${data.iban}</td>
+            </tr>
+            <tr>
+                <td>Account Holder:</td>
+                <td>${data.accountHolder.name}</td>
+            </tr>
+            <tr>
+                <td>Address: </td>
+                <td>${data.accountHolder.address}</td>
+            </tr>
+            <tr>
+                <td>Geographical Region:</td>
+                <td>${data.geoRegion}</td>
+            </tr>
+            </tbody>
+        </table>`;
+
+        $('#data-table').html(htmlTable);
+    };
+
+    const error = function (data) {
+        $("#error").show();
+        $("#error .error-text").text("Failed to load account details (" + data.statusText + " [" + data.status + "])");
+    }
+    $.ajax({
+        type: "GET",
+        url: "/account/" + $('#account-iban-input').val() + "/details",
+        success: ready,
+        error: error
+    })
+});
+
+$('#account-transactions').click(function () {
+    const ready = function (data) {
+        $("#error").hide();
+
+        const tr = (t) => `<tr>
+            <td>${t.otherAccountIban}</td>
+            <td>${t.amount}</td>
+            <td>${t.timeStamp}</td>
+            <td>${t.type}</td>
+            <td>${t.result}</td>
+            </tr>`
+        const htmlRows = data.transactionList.map(tr).join('')
+
+        const htmlTable = `<table class="table table-bordered">
+            <thead>
+                <tr>
+                  <th scope="col">Other Account</th>
+                  <th scope="col">Amount</th>
+                  <th scope="col">Time</th>
+                  <th scope="col">Type</th>
+                  <th scope="col">Result</th>
+                </tr>
+            </thead>
+            <tbody>
+                ${htmlRows}
+            </tbody>
+        </table>`;
+
+        $('#data-table').html(htmlTable);
+    };
+
+    const error = function (data) {
+        $("#error").show();
+        $("#error .error-text").text("Failed to load account transactions (" + data.statusText + " [" + data.status + "])");
+    }
+    $.ajax({
+        type: "GET",
+        url: "/account/" + $('#account-iban-input').val() + "/transactions",
+        success: ready,
+        error: error
+    })
+});
 
 $(readyFn)
 

380f22e16a32970f17337e42112cbd863651a34a
honeybadger10 <geigerd@gmx.net>
2022-01-13 10:21:38 +0100

    load and purge policies

--- provisioning/das/1-create-git-secret.sh
+++
@@ -1,5 +0,0 @@
-curl  -H "Authorization: Bearer $API_TOKEN" -H 'Content-Type: application/json' \
-      -X PUT https://adamsandor.svc.styra.com/v1/secrets/github -d "{
-    \"name\": \"adam-sandor\",
-    \"secret\": \"$(cat ../config/github-token)\"
-}"
